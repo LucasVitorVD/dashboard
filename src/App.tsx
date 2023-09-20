8d6b2eaba2515196baee691e3478b3509d15091f
LucasVitorVD <lucaskirito85@gmail.com>
2023-09-19 21:30:01 -0300

    feat: implement mobile header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
-import { LayoutGrid, Import, ShoppingCart, Users, Settings, Power, Menu, X } from "lucide-react"
+import { LayoutGrid, Import, ShoppingCart, Users, Settings, Power, Menu, X, Search, Bell } from "lucide-react"
 import { Separator } from "@/components/ui/separator"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import MenuItem from "./components/MenuItem"
 
 const App = () => {
@@ -53,13 +54,39 @@ const App = () => {
           />
         </div>
       </aside>
-      <main className="bg-[#F3F4F8] flex-1">
-        <Menu 
-          size={24} 
-          className="text-brandColor md:hidden"
-          onClick={() => setIsOpen(true)} 
-        />
-        <h1>Content</h1>
+      <main className={`bg-darkScale-dark30 flex flex-col flex-1 transition-all ease-in-out ${open ? "blur-sm" : "filter-none"} md:filter-none`}>
+        <header className="flex items-center justify-between p-7 md:hidden">
+          <Menu
+            size={24}
+            className="text-brandColor md:hidden"
+            onClick={() => setIsOpen(true)}
+          />
+
+          <div className="flex items-center gap-6">
+            <Search size={24} />
+            <div className="relative">
+              <span className="absolute flex h-2 w-2">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-brandColor opacity-75 left-5 top-[-3px]"></span>
+                <span className="relative inline-flex rounded-full h-2 w-2 bg-brandColor left-5 top-[-3px]"></span>
+              </span>
+              <Bell size={24} />
+            </div>
+            <Avatar className="ml-6">
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+        </header>
+        <section className="flex-1 px-6 py-8 md:px-44 md:py-12">
+          <h1>Olá, Iuri</h1>
+          <p>Aqui estão as informações sobre suas vendas.</p>
+
+          {/* grid dashboard */}
+          <div></div>
+
+          {/* history Table */}
+          <div></div>
+        </section>
       </main>
     </div>
   )
